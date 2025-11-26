9fc9789447028413d779f976a6fe8096f514465f
fabianprz147 <233898254+fabianprz147@users.noreply.github.com>
2025-11-25 21:27:16 -0600

    feat: crea utils.py, contiene función de log que modulariza el registro de eventos y errores.

---
+++ utils.py
@@ -0,0 +1,32 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s",
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    handlers=[logging.FileHandler("qts.log"), logging.StreamHandler()])
+
+def log_status(message:str, status:str="info") -> None:
+    '''Registra mensajes de estado en el log.
+    
+    Args:
+        message (str): El mensaje a registrar, viene directo del módulo en ejecución.
+        status (str): El nivel de log ('info', 'error', 'warning'), también viene directo del módulo en ejecución.
+    Returns:
+        None
+    '''
+
+    colors = {
+        "info": "\033[92mOK\033[0m",
+        "error": "\033[91mError\033[0m",
+        "warning": "\033[93mWarning\033[0m"
+    }
+
+    prefix = f"[{colors.get(status, '')}]"
+
+    if status == "info":
+        logging.info(f"{prefix} {message}")
+    elif status == "error":
+        logging.info(f"{prefix} {message}")
+    elif status == "warning":
+        logging.info(f"{prefix} {message}")
+    else:
+        logging.info(f"{prefix} {message}")
